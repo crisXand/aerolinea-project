0c3c44f12f9972ee58ca225c147e28b7a0fde99f
crisXand05 <cristianmndz009@gmail.com>
2023-04-01 23:35:17 -0600

    Empleado and cargo models

--- aplicaciones/empleado/models.py
+++ aplicaciones/empleado/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Cargo(models.Model):
+    idcargo = models.BigAutoField(primary_key=True) #django coloca el id implicito si no se escribe
+    cargo = models.CharField('Cargo', max_length=50)
+
+    def __str__(self) :
+        return self.cargo
+    
+
+class Empleado(models.Model):
+    nombre = models.CharField('Nombre', max_length=50)
+    correo = models.CharField('Correo', max_length=100)
+    sueldo = models.DecimalField('Sueldo', max_digits=8, decimal_places=2)
+    activo = models.BooleanField('Activo')
+    cargo = models.ForeignKey(Cargo, on_delete=models.RESTRICT)
+
+    def __str__(self):
+        return self.nombre + '|' + self.correo + '|'+ self.activo
