643e3bcb7418ca973cdce2e303e1893630ba393e
crisXand05 <cristianmndz009@gmail.com>
2023-04-01 23:30:47 -0600

    List person

--- aplicaciones/inicio/views.py
+++ aplicaciones/inicio/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def inicio(request):
     return render(request, 'inicio/index.html')
+
+def listar_personas(request):
+    lista_personas = {
+        "titulo": 'Lista de personas',
+        "personas": [{'nombre':'juan','edad':12}, {'nombre':'jose','edad':23}, {'nombre':'juan','edad':12}]
+    }
+    return render(request, "inicio/listar_personas.html",lista_personas)
