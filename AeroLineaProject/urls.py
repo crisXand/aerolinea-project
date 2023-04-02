e04b874c616504da1beeadcabbd2746c70b40de1
crisXand05 <cristianmndz009@gmail.com>
2023-04-01 23:41:09 -0600

    list person url

--- AeroLineaProject/urls.py
+++ AeroLineaProject/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('aplicaciones.inicio.urls'))
+    path('inicio/', include('aplicaciones.inicio.urls'))
 ]
